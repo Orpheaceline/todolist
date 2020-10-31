0e6a3460b399c95eb1a4a1b29357aed018d541d7
Delcampe - Céline <ccrabeels@delcampe.local>
2020-10-31 12:41:01 +0100

    swipe

---
+++ todolist-vuejs/src/pages/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <section>
+    <Navigation name="Home" />
+    <div class="p-lg">
+      <h1 class="logo"><span>Ma Todo Liste</span></h1>
+      <Todolist id="MyTodoListApp" name="Home" />
+    </div>
+  </section>
+</template>
+
+<script>
+import Navigation from 'components/Navbar'
+import Todolist from 'components/Todolist'
+
+export default {
+  name: 'Home',
+  components: {
+    Navigation,
+    Todolist
+  }
+}
+</script>
